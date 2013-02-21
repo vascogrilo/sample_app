e25c499792244e589e92761bdb1cf14730887c54
vascogrilo <vascogrilo@gmail.com>
2013-02-21 17:43:27 +0000

    Exercise 2 from 3.5: Refactor test specs for the title.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,55 +2,49 @@ require 'spec_helper'
 
 describe "StaticPages" do
   
+  let(:base_title) { "Ruby on Rails Tutorial Sample App"}
+
   describe "Home page" do
     it "should have the content 'Sample App'" do
     	visit '/static_pages/home'
-    	page.should have_selector('h1',
-    		:text => "Sample App")
+    	page.should have_selector('h1', :text => "Sample App")
     end
     it "should have the right title" do
     	visit '/static_pages/home'
-    	page.should have_selector('title',
-    		:text => "Ruby on Rails Tutorial Sample App | Home")
+    	page.should have_selector('title', :text => "#{base_title} | Home")
     end
   end
 
   describe "Help Page" do
   	it "should have the content 'Help'" do
   		visit '/static_pages/help'
-  		page.should have_selector('h1',
-  			:text => "Help")
+  		page.should have_selector('h1', :text => "Help")
   	end
   	it "should have the right title" do
   		visit '/static_pages/help'
-  		page.should have_selector('title',
-  			:text => "Ruby on Rails Tutorial Sample App | Help")
+  		page.should have_selector('title', :text => "#{base_title} | Help")
   	end
   end
 
   describe "About Page" do
   	it "should have the content 'About Us'" do
   		visit '/static_pages/about'
-  		page.should have_selector('h1',
-  			:text => "About Us")
+  		page.should have_selector('h1', :text => "About Us")
   	end
   	it "should have the right title" do
   		visit '/static_pages/about'
-  		page.should have_selector('title',
-  			:text => "Ruby on Rails Tutorial Sample App | About Us")
+  		page.should have_selector('title', :text => "#{base_title} | About Us")
   	end
   end
 
   describe "Contact Page" do
     it "should have the content 'Contact Us'" do
       visit '/static_pages/contact'
-      page.should have_selector('h1',
-        :text => "Contact Us")
+      page.should have_selector('h1', :text => "Contact Us")
     end
     it "should have the right title" do
       visit '/static_pages/contact'
-      page.should have_selector('title',
-        :text => "Ruby on Rails Tutorial Sample App | Contact Us")
+      page.should have_selector('title', :text => "#{base_title} | Contact Us")
     end
   end
 
