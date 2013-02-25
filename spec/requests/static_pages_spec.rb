75ec9442865d1ab460dad850ffb2f7ed6e97b584
vascogrilo <vascogrilo@gmail.com>
2013-02-25 23:14:09 +0000

    Something

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,18 +5,22 @@ describe "StaticPages" do
   let(:base_title) { "Ruby on Rails Tutorial Sample App"}
 
   describe "Home page" do
-    it "should have the content 'Sample App'" do
+    it "should have the h1 'Sample App'" do
     	visit '/static_pages/home'
     	page.should have_selector('h1', :text => "Sample App")
     end
     it "should have the right title" do
     	visit '/static_pages/home'
-    	page.should have_selector('title', :text => "#{base_title} | Home")
+    	page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App")
+    end
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => '| Home')
     end
   end
 
   describe "Help Page" do
-  	it "should have the content 'Help'" do
+  	it "should have the h1 'Help'" do
   		visit '/static_pages/help'
   		page.should have_selector('h1', :text => "Help")
   	end
@@ -27,7 +31,7 @@ describe "StaticPages" do
   end
 
   describe "About Page" do
-  	it "should have the content 'About Us'" do
+  	it "should have the h1 'About Us'" do
   		visit '/static_pages/about'
   		page.should have_selector('h1', :text => "About Us")
   	end
